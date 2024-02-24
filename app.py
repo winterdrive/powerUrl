a69e68eb91551577a79ef06ece256124b3769909
winterdrive <90021888+winterdrive@users.noreply.github.com>
2024-02-24 23:33:45 +0800

    add LINE bot APIs

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
-import os
+import random
+import string
+import traceback
 
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, abort, redirect
+from linebot import LineBotApi, WebhookHandler
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from os import environ
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+line_bot_api = LineBotApi(
+    environ.get("LINE_BOT_API_TOKEN")
+)
+handler = WebhookHandler(
+    environ.get("LINE_BOT_CHANNEL_SECRET")
+)
+
+url_mapping = {}
 
 
 @app.route('/hello', methods=['GET'])
@@ -11,34 +27,53 @@ def hello_world():
 
 
 def generate_short_url():
-    import random
-    import string
     characters = string.ascii_letters + string.digits
     short_url = ''.join(random.choice(characters) for i in range(6))
     return short_url
-
-
-url_mapping = {}
-
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        long_url = request.form['long_url']
-        short_url = generate_short_url()
-        url_mapping[short_url] = long_url
-        return render_template('index.html', short_url=f'{request.host_url}{short_url}')
-    return render_template('index.html', short_url=None)
 
 
 @app.route('/<short_url>')
 def redirect_to_long_url(short_url):
     if short_url in url_mapping:
         long_url = url_mapping[short_url]
-        return redirect(long_url)
+        return redirect(long_url, code=302)
     else:
         return "URL not found", 404
 
 
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    user_message = event.message.text
+    if user_message.startswith("Shorten:"):
+        long_url = user_message.split(":", 1)[1].strip()
+        short_url = generate_short_url()
+        url_mapping[short_url] = long_url
+        reply_message = f"Shortened URL: {request.host_url}{short_url}"
+    else:
+        reply_message = "Invalid command. Please start your message with 'Shorten:' followed by the URL you want to " \
+                        "shorten."
+
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=reply_message)
+    )
+
+
+@app.route("/callback", methods=["POST"])
+def callback():
+    signature = request.headers["X-Line-Signature"]
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    try:
+        handler.handle(body, signature)
+    except Exception as e:
+        traceback.print_exc()
+        print(e)
+        abort(400)
+
+    return "OK"
+
+
 if __name__ == '__main__':
-    app.run(port=os.getenv("PORT", default=5000), debug=True)
+    app.run(port=environ.get("PORT", default=5000), debug=True)
